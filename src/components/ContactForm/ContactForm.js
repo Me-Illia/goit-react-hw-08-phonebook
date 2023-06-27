bbeaf43ca9ae12558c5dbdc5f1d7a6d23c9f0904
Me-Illia <sq999@ukr.net>
2023-06-27 19:48:18 +0300

    Update ContactForm.js

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -1,29 +1,45 @@
+import { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { addContact } from '../../redux/operations';
 import { selectContacts } from '../../redux/selectors';
 import { Form, Label } from './ContactForm.styled';
 
 const ContactForm = () => {
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
+
   const contacts = useSelector(selectContacts);
   const dispatch = useDispatch();
 
   function handleSubmit(e) {
     e.preventDefault();
-    const form = e.target;
-    const name = form.name.value;
-    const number = form.number.value;
 
     if (contacts.find(
       contact => contact.name.toLowerCase() === name.toLowerCase()
     )) {
       alert(`"${name} already in contacts"`);
-      form.reset();
       return;
     }
 
     dispatch(addContact({ name, number }));
-    form.reset();
+    setName('');
+    setNumber('');
   }
+
+  const handleChange = event => {
+    const { name, value } = event.target;
+
+    switch (name) {
+      case 'name':
+        setName(value);
+        break;
+      case 'number':
+        setNumber(value);
+        break;
+      default:
+        return;
+    }
+  };
 
   return (
     <Form onSubmit={handleSubmit}>
@@ -32,6 +48,7 @@ const ContactForm = () => {
         <input
           name="name"
           type="text"
+          onChange={handleChange}
           pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
           title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
           required={true}
@@ -43,6 +60,7 @@ const ContactForm = () => {
         <input
           type="tel"
           name="number"
+          onChange={handleChange}
           pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
           title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
           required={true}
