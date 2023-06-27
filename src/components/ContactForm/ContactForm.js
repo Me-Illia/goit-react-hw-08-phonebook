5f45e1ebf663e2c31f05c8acac943feee0ebdb16
Me-Illia <sq999@ukr.net>
2023-06-27 21:34:02 +0300

    Update ContactForm.js

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -66,7 +66,7 @@ const ContactForm = () => {
           pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
           title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
           required={true}
-          placeholder="Enter phone numbe"
+          placeholder="Enter phone number"
         />
       </Label>
 
